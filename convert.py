d84796993a317c2aeb48e1f957bda3d19221f97c
Alan Sparrow <git@freeflight.org.uk>
2021-07-19 14:39:14 +0100

    Change name to place

--- convert.py
+++ convert.py
@@ -8,17 +8,17 @@ def parse(dove_file):
     reader = csv.DictReader(dove_file)
     for tower in reader:
         # There isn't really a satisfactory tower name in the Dove data
-        tower_name = tower['PlaceCL'] or tower['Place']
+        place = tower['PlaceCL'] or tower['Place']
 
         # Skip Carillons
         if tower['RingType'] != "Full circle ring":
-            print("Carillon: " + tower_name, file=sys.stderr)
+            print("Carillon: " + place, file=sys.stderr)
             continue
 
         # Skip unassigned or temporary TowerBase IDs
         tower_base = int(tower['TowerBase'])
         if tower_base == 0 or tower_base >= 9000:
-            print("Temporary TowerBase: " + tower_name, file=sys.stderr)
+            print("Temporary TowerBase: " + place, file=sys.stderr)
             continue
 
         # Skip mobile rings
@@ -26,7 +26,7 @@ def parse(dove_file):
             latitude = float(tower['Lat'])
             longitude = float(tower['Long'])
         except ValueError:
-            print("Mobile ring: " + tower_name, file=sys.stderr)
+            print("Mobile ring: " + place, file=sys.stderr)
             continue
 
         # Use country where county isn't specified
@@ -34,7 +34,7 @@ def parse(dove_file):
 
         towers.append({
                 'id': tower_base,
-                'name': tower_name,
+                'place': place,
                 'dedication': tower['Dedicn'],
                 'county': county,
                 'latitude': latitude,
